9e3fa0755a7b4b31831b4f7afa0baeafa382d23c
David Chen <qydchen@gmail.com>
2020-08-14 00:04:11 -0400

    setup express with mongoose and ejs template

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/test");
+
+app.set("port", process.env.PORT || 3000);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+    session({
+        secret: "wtf",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use(flash());
+
+app.use(routes);
+
+app.listen(app.get("port"), function () {
+    console.log("Server started on port " + app.get("port"));
+});
